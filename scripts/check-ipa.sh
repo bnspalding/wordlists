32b2a060d21f5b02a7e7e83a4dae0cdeef711752
Ben Spalding <bnspalding418@gmail.com>
2020-09-11 12:12:01 -0400

    outline for checking pronunciation files

---
+++ scripts/check-ipa.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# check pronunciation files against each other
+
+# For each file:
+#   no leading or trailing space in a pronunciation file
+#   1 tab should separate word and pronunciation information
+#   monosyllabic words should not have a stress mark
+#   files are sorted
+
+# For all files:
+#   pronunciation information matches (sometimes you don't want this?)
+
+
