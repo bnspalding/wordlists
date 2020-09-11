e8abf401067155e9f2890964eb81efcfd47708e8
Ben Spalding <bnspalding418@gmail.com>
2020-09-11 15:56:56 -0400

    copy pronunciations script; Fixes #4

--- scripts/copy-pronunciations.sh
+++ scripts/copy-pronunciations.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Copy pronunciation information to a new word list
 
+WORDLIST=$1
+PRONUNC=$2
+
 # join wordlist (f1) with pronunciations from f2
+{ join $PRONUNC $WORDLIST \
+  & join -v2 $PRONUNC $WORDLIST; } \
+  | sort | sed 's/ /	/g'
